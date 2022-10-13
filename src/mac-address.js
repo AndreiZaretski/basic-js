dc2731c22d33f2e2e83ba4ad73527350d6b6338d
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2022-10-13 12:06:48 +0300

    feat: solution5-

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,72 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(n) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+// let res1 = n.split('-');
+
+// if(res1.length !==6) return false;
+// if (res1.length ===6) {
+//   for(let i=0; i<6; i++) {
+//     let res2 = res1[i].split('');
+//     if(res2.length ===2){
+      
+//         if(res2[0]==='0'||'1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'A'||'B'||'C'||'D'||'E'||'F' 
+//           && res2[1]==='0'||'1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'A'||'B'||'C'||'D'||'E'||'F') 
+//           {return true} 
+       
+//     } 
+//   } 
+// }else {return false}
+// console.log(res1, res2)
+
+// let valid = true;
+// for (let i=0; i<n.length; i++)
+// {
+    
+//     let regex = /^([A-F|0-9]{2}-){5}[A-F|0-9]{2}$/;
+//     if (!regex.test(n[i]))
+//     {
+//  valid = false;
+//  break;
+//     }
+// }
+// return valid;
+
+let valid = true;
+//Проверка входного параметра
+if (!n instanceof Array || n.length == 0)
+   return false;
+             
+for (let i=0; i<n.length; i++)
+{
+   // Разбиение mac-адреса на элементы по символу ':'
+   // elementsMacAddress - массив такого формата:
+   //    mac-адрес: 64:A2:C7:B0:D3:B2
+   //    массив: ['64','A2','C7','B0','D3','B2']
+   let elementsMacAddress = n[i].split('-');
+   // В массиве должно быть ровно шесть элементов mac-адреса
+   if (elementsMacAddress.length == 6)
+   {
+       var regex = /^[A-F|0-9]{2}$/;
+       // Перебор всех компонентов mac-адреса
+       for (let j=0; j<elementsMacAddress.length; j++)
+       {
+    if (!regex.test(elementsMacAddress[j]))
+           {
+        valid = false;
+        return valid;
+    }
+       }
+   }
+   else
+   {
+       valid = false;
+       break;
+   }
+}
+return valid;
 }
 module.exports = {
   isMAC48Address
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let month= date.getMonth();
+  let res;
+  if( month == 11 || 0||1) {res = 'winter'}
+  else if (month == 2||3||4) {res = 'spring'}
+  else if (month ==5 ||6||7) {res ='summer'}
+  else if (month = 8||9||10){res == 'autumn'}
+  else if (typeof(month) != Number) {res == 'Invalid date!'}
+
+  return res
 }
 
 module.exports = {
