55e757722de2eaa06a8acac207ae8f7da4787cb0
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2022-10-17 20:01:37 +0300

    feat: solution dns-stats and get-email-domain

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -23,30 +23,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function getDNSStats(domains) {
-  // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-
-  // let x = domains.map((elem)=>{
-  //   elem.split('.').reverse();
-  // });
-
-  // let res1 =`.+${x[0][0].join('')}`;
-  // let res2 = `${res1}+.+${x[0][1].join('')}`
-  // let res3 = `${res2} +.+${x[1][2].join('')}`
-
-  // for( i=0; i<domains.length; i++){
-  //   for (j=0; j<x.length; j++){
-  //     let 
-  //   }
-  // }
+  let result = {};
+  
   for(i=0; i<domains.length; i++) {
+    let newDomains = domains[i].split('.').reverse();
+    let value = '';
+    newDomains.forEach(element => {
+      value += `.${element}`;
+     if (result[value]) { 
+      result[value]++; 
+    } else {
+      result[value] = 1;
+    }
+      
+    });
     
   }
- let result = {
-  res1: x.length,
-  res2: x.length,
-  res3: 1
- }
+ 
  return result
 }
 
